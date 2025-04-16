abd5fbc84adb1a8281f2c70c257685366f1c9dde
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-04-16 16:09:22 -0300

    Update App.jsx

--- newsletter/src/App.jsx
+++ newsletter/src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
 import NewsletterForm from './components/NewsletterForm'
+import IlustrationMobile from './assets/images/illustration-sign-up-mobile.svg'
 
 function App() {
 
   return (
     <>
-      <NewsletterForm />
+      <main className='newsletter-container'>
+        <img src={IlustrationMobile} alt="ilustração de gráficos" />
+
+        <section className='text-content'>
+          <h1>título</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et qui nostrum veniam harum praesentium perspiciatis ipsum eaque at? Voluptas eaque cum magnam ratione esse. Iste labore alias officiis assumenda quisquam!
+          </p>
+          
+          <ul>
+            <li>
+              item 1              
+            </li>
+            <li>
+              item 2
+            </li>
+            <li>
+              item 3
+            </li>
+          </ul>
+        </section>
+
+          <NewsletterForm />
+      </main>
     </>
   )
 }
