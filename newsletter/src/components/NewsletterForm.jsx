355eae965d944de815e2fa62fa1857c03e71fc82
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2025-04-18 15:48:37 -0300

    Configurações de estilo formulário
    mensagem de erro e caixa do input e-mail em caso de erro

--- newsletter/src/components/NewsletterForm.jsx
+++ newsletter/src/components/NewsletterForm.jsx
@@ -3,26 +3,58 @@ import './NewsletterForm.css'
 
 const NewsletterForm = () => {
   const [email, setEmail] = useState("")
+  const [error, setError] = useState("")
 
-  const handleEmail = (e) => setEmail(e.target.value)
+  //atualiza o estado com o valor digitado
+  const handleEmail = (e) => {
+    setEmail(e.target.value) 
+    setError("") //Limpa o erro ao começar a digitar novamente
+  }
 
-  console.log(email)
+  //Função de validação do e-mail, utilizando Regex
+  const validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return regex.test(email)
+  }
 
+  //Envia o formulário se estiver tudo Ok
   const handleSubmit = (e) => {
     e.preventDefault()
+    
+    if (!email || !validateEmail(email)) {
+      setError("Adicione um e-mail válido")
+      return
+    }
 
     console.log("Enviando formulário...")
     console.log(`E-mail cadastrado: ${email}`)
 
-    //limpar campo do formulário após o envio
+    //limpar campo do formulário e o erro (caso haja) após o envio.
+    setError("")
+    setEmail("")
   }
 
   return (
     <>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">Endereço de e-mail</label>
-            <input type="email" name='email' placeholder='email@company.com' onChange={handleEmail} />
+            <div className='label-error-wrapper'>
+              <label htmlFor="email">Endereço de e-mail</label>
+              {/*Adiciona mensagem de erro em caso de e-mail invalido ou vazio*/}
+              {error && <span id='email-error' className="error-message" role='alert'>{error}</span>}
+            </div>
+
+            <input 
+              type="text" 
+              name='email' 
+              placeholder='email@company.com' 
+              value={email} 
+              onChange={handleEmail} 
+              aria-invalid={!!error} 
+              aria-describedby={error ? "email-error" : undefined}
+              className={error ? "error-config" : "input-email"} 
+            />
+            
           </div>
 
           <div>
